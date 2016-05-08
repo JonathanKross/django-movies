22ff11ff7558c9b17e33f9870c7da916a5ac591c
JonathanKross <kross.jonathan+github@gmail.com>
2016-05-08 16:17:23 -0400

    Updating views in admin page. Adding list_display on admin.py

--- movieratings/lensview/admin.py
+++ movieratings/lensview/admin.py
@@ -3,15 +3,13 @@ from .models import Movie, Rater, Rating
 
 
 class RaterAdmin(admin.ModelAdmin):
-    pass
-
+    list_display = ('user', 'gender', 'age', 'occupation', 'zipcode')
 
 class MovieAdmin(admin.ModelAdmin):
-    pass
-
+    list_display = ('title', 'id', 'genre', 'average_rating')
 
 class RatingAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('rater','movie', 'stars')
 
 
 admin.site.register(Rater, RaterAdmin)
