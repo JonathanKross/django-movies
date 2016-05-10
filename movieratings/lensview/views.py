d1a002a6f962df682e90554849759c94a0bd61ee
The Iron Yard <theironyard@Thes-MacBook-Air.local>
2016-05-10 15:32:26 -0400

    Adding simple 404 Error to views.py

--- movieratings/lensview/views.py
+++ movieratings/lensview/views.py
@@ -20,13 +20,13 @@ def top_movies(request):
 
 
 def show_movie(request, movie_id):
-    movie = Movie.objects.get(pk=movie_id)
+    movie = get_object_or_404(Movie, pk=movie_id)
     ratings = movie.rating_set.all()
     return render(request, 'lensview/movie.html', {'movie': movie, 'ratings': ratings})
 
 
 def show_rater(request, rater_id):
-    rater = Rater.objects.get(pk=rater_id)
+    rater = get_object_or_404(Rater, pk=rater_id)
     ratings = rater.rating_set.all()
     return render(request, 'lensview/rater.html', {'rater': rater, 'ratings': ratings})
 
